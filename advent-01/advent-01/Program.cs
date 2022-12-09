5462fee8c22a75a1e0c1232629bc874601298be6
bvshi <bvshibvshi@gmail.com>
2022-12-09 20:35:28 +0100

    Initial commit

---
+++ advent-01/advent-01/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace advent_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("sample.txt");
+            List<string> caloriesList = new List<string>(data);
+
+            int caloriesSum = 0;
+            int maxCalories = 0;
+            foreach (string calories in caloriesList)
+            {
+                if (calories == string.Empty)
+                {
+                    maxCalories = Math.Max(maxCalories, caloriesSum);
+                    Console.WriteLine($"Total calories: {caloriesSum}");
+                    Console.WriteLine($"Current max val: {maxCalories}");
+
+                    caloriesSum = 0;
+
+                }
+
+                else
+                {
+                    int caloriesAmount = int.Parse(calories);
+                    caloriesSum += caloriesAmount;
+                    Console.WriteLine(caloriesAmount);
+                }
+            }
+            maxCalories = Math.Max(maxCalories, caloriesSum);
+            Console.WriteLine($"Total calories: {caloriesSum}");
+            Console.WriteLine($"Max calories value is {maxCalories}");
+        }
+
+    }
+}
